11ddcb9c749d438d44baebde764781d411dbc4c9
Syed Humza Shah <humzashah@gmail.com>
2016-06-04 12:23:56 +0100

    completed hour 6

---
+++ hours/h06l01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main (void) {
+  int a = 1;
+
+  /* one data type can be cast as another using (new type)variable */
+  printf("Print integer as float: %f\n", (float)a);
+
+  exit(0);
+}
