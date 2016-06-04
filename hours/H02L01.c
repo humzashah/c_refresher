e75beaba1c22bb9979ba450053e6713732ef0874
Syed Humza Shah <humzashah@gmail.com>
2016-06-04 07:59:19 +0100

    completed hour 2

---
+++ hours/H02L01.c
@@ -0,0 +1,8 @@
+#include <stdlib.h> /* for exit(0) */
+#include <stdio.h> /* for printf */
+
+void main() /* void indicates function doesn't return value */
+{
+  printf ("Howdy, neighbor! This is my first C program.\n");
+  exit(0);
+}
