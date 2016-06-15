258dc8666c5b861ee9de4162d9181ee2e3c8f0f1
Syed Humza Shah <humzashah@gmail.com>
2016-06-15 11:05:26 +0100

    completed enums

--- samples/s017-enum.c
+++ samples/s017-enum.c
@@ -5,9 +5,15 @@
 
 void main (void)
 {
+  /*
+    - enumerations help in writing clear and simplified codes
+    - by default enum values start with 0
+    - this can be changed by either changing the first value or by assigning
+      all values
+  */
   enum DAY
   {
-    monday = 0,
+    monday = 1,
     tuesday,
     wednesday,
     thursday,
@@ -19,7 +25,7 @@ void main (void)
   enum DAY today = wednesday;
 
   printf("today's day number is: %d\n", today);
-  /* Output: => today's day number is: 2 */
+  /* Output: => today's day number is: 3 */
 
   exit(0);
 }
